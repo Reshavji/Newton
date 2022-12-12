f3a59716c58d5a81c4aa395e1270da45e5405121
resha <reshavkumar9837@gmail.com>
2022-12-12 14:27:01 +0530

    hi

---
+++ src/Dsa1/Bubble_sort.java
@@ -0,0 +1,20 @@
+package Dsa1;
+
+public class Bubble_sort {
+    public static void main(String args[]){
+        int arr [] = {2,6,1,4,8,11,1,2};
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            for (int j =0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for (int i =0;i<n;i++){
+            System.out.print(arr[i] +" ");
+        }
+    }
+}
